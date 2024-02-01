cb00338da373ba5b0d1aebfb065b41af3a60855a
ArchanaVyas <archana.vyas@bacancy.com>
2024-02-01 17:11:33 +0530

    Add project files.

---
+++ NetTrainingJan/Middleware/mid.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace NetTrainingJan.Middleware
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class Middleware1
+    {
+        private readonly RequestDelegate _next;
+
+        public Middleware1(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            Console.WriteLine("Middleware Training");
+            Console.WriteLine("Hello Archana Mid1 sending request");
+            await _next(httpContext);
+            Console.WriteLine("Mid1 response");
+
+        }
+    }
+
+    public class Middleware2
+    {
+        private readonly RequestDelegate _next;
+
+        public Middleware2(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext)
+        {
+            Console.WriteLine("Middleware2");
+            Console.WriteLine("Mid2 sending request");
+            await _next(httpContext);
+            Console.WriteLine("Mid2 response");
+
+        }
+    }
+
+
+    // Extension method used to add the middleware to the HTTP request pipeline.
+
+
+    public static class MiddlewareExtensions
+    {
+        public static IApplicationBuilder UseCustomMiddlewares(this IApplicationBuilder app)
+        {
+            return app
+                .UseMiddleware<Middleware1>()
+                .UseMiddleware<Middleware2>();
+        }
+    }
+
+}
